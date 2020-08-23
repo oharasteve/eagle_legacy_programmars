c083cb95c675609a8cf8518f08f05ff418ce8fb5
Steve O'Hara <steve@oharasteve.com>
2020-08-23 13:04:27 -0500

    Bogus </a> still not working

--- src/com/eagle/programmar/HTML/HTML_Anchor.java
+++ src/com/eagle/programmar/HTML/HTML_Anchor.java
@@ -3,10 +3,10 @@
 
 package com.eagle.programmar.HTML;
 
-import com.eagle.programmar.HTML.HTML_Anchor.HTML_AnchorHref.HTML_EndAnchor;
 import com.eagle.programmar.HTML.HTML_Attribute.HTML_Value;
 import com.eagle.programmar.HTML.Terminals.HTML_Keyword;
 import com.eagle.programmar.HTML.Terminals.HTML_Punctuation;
+import com.eagle.programmar.HTML.Terminals.HTML_PunctuationChoice;
 import com.eagle.programmar.PHP.PHP_Program.PHP_Entry;
 import com.eagle.tokens.TokenChooser;
 import com.eagle.tokens.TokenList;
@@ -15,10 +15,6 @@ import com.eagle.tokens.punctuation.PunctuationEquals;
 
 public class HTML_Anchor extends TokenChooser
 {
-	// Note: If you have an <a name=>stuff</a> it will get picked up by HTML_AnchorHref first.
-	// If there is no </a> but there is an <a name=> it will get picked up @LAST with HTML_AnchorName
-	// This is fairly tricky because there is no way to say that we can't have an anchor inside an anchor.
-	
 	public @CHOICE static class HTML_AnchorHref extends TokenSequence
 	{
 		public @INDENT HTML_StartAnchor startTagA;
@@ -41,26 +37,19 @@ public class HTML_Anchor extends TokenChooser
 		}
 	}
 
+	// Tried all sorts of things to get this to work. No dice.
+	// Test case 1: /www/rrcc\lh_tr_2006\7B.htm (times out if above catches the name. No bogus entries)
+	// Test case 2: /wwwa/Dox/Supplier_Portal/SupplierSearch/SupplierSearch/help.html (has bogus entries)
+	// public @LAST @CURIOUS("Extra end anchor name") HTML_EndAnchor bogusAnchorNameEnd;
+
 	// This is an oddball case. An anchor with no closing </a> (in theory).
-	public @LAST static class HTML_AnchorName extends TokenSequence
+	public @FIRST static class HTML_AnchorName extends TokenSequence
 	{
 		public @INDENT HTML_Punctuation startTagA = new HTML_Punctuation('<');
 		public @NOSPACE HTML_Keyword A = new HTML_Keyword("a");
 		public HTML_Keyword NAME = new HTML_Keyword("name");
 		public @NOSPACE PunctuationEquals equals;
 		public @NOSPACE HTML_Value value;
-		public @NOSPACE HTML_EndAnchorName endAnchorName;
-		
-		public static class HTML_EndAnchorName extends TokenChooser
-		{
-			public @CHOICE HTML_Punctuation endTagA = new HTML_Punctuation("/>");
-			
-			public static @CHOICE class HTML_EndAnchorNameOddity extends TokenSequence
-			{
-				public HTML_Punctuation endTagA = new HTML_Punctuation(">");
-				// This probably never happens because it will have been picked up by HTML_AnchorHref above.
-				public @OPT @CURIOUS("Extra end anchor name") HTML_EndAnchor bogusAnchorNameEnd;
-			}
-		}
+		public @NOSPACE HTML_PunctuationChoice endTag = new HTML_PunctuationChoice("/>", ">");
 	}
 }
