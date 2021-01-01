d7fcc5a449d1ba0dbad4e0571ec9f23b71f25fab
Steve O'Hara <steve@oharasteve.com>
2020-12-31 23:09:56 -0600

    Unused file (so far). Trying to combine all simple tokens into one long string (inlcuding newlines)

---
+++ src/com/eagle/programmar/CMacro/Terminals/CMacro_MultiLineText.java
@@ -0,0 +1,40 @@
+// Copyright Eagle Legacy Modernization LLC, 2010-date
+// Original author: Steven A. O'Hara, Oct 11, 2015
+
+package com.eagle.programmar.CMacro.Terminals;
+
+import com.eagle.parsers.EagleFileReader;
+import com.eagle.parsers.EagleLineReader;
+import com.eagle.tokens.TerminalLiteralToken;
+
+public class CMacro_MultiLineText extends TerminalLiteralToken
+{
+	@Override
+	public boolean parse(EagleFileReader lines)
+	{
+		if (findStart(lines) != FOUND.GOOD) return false;
+		StringBuffer text = new StringBuffer();
+		int endLine = _currentLine;
+		EagleLineReader firstRec = lines.get(_currentLine);
+		int recLen = firstRec.length();
+		if (recLen < _currentChar) return false;
+
+		while (endLine < lines.size())
+		{
+			EagleLineReader rec = lines.get(endLine);
+			recLen = rec.length();
+			endLine++;
+			
+			// Don't allow lines that start with a #
+			if (rec.trim().startsWith("#")) break;
+			
+			if (text.length() > 0) text.append('\n');
+			text.append(rec);
+		}
+		if (text.length() == 0) return false;
+		
+		_txt = text.toString();
+		foundIt(endLine-1, recLen-1);
+		return true;
+	}
+}
