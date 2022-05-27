fbea99538e1698955dc4248acb6161a7e6856e27
Steve O'Hara <steve@oharasteve.com>
2022-05-26 22:40:46 -0500

    Improve Delphi 'uses' statement

--- src/com/eagle/programmar/Delphi/Delphi_Uses.java
+++ src/com/eagle/programmar/Delphi/Delphi_Uses.java
@@ -5,14 +5,37 @@ package com.eagle.programmar.Delphi;
 
 import com.eagle.programmar.Delphi.Symbols.Delphi_Identifier_Reference;
 import com.eagle.programmar.Delphi.Terminals.Delphi_Keyword;
+import com.eagle.programmar.Delphi.Terminals.Delphi_Literal;
 import com.eagle.tokens.SeparatedList;
 import com.eagle.tokens.TokenSequence;
 import com.eagle.tokens.punctuation.PunctuationComma;
+import com.eagle.tokens.punctuation.PunctuationLeftBrace;
+import com.eagle.tokens.punctuation.PunctuationRightBrace;
 import com.eagle.tokens.punctuation.PunctuationSemicolon;
 
 public class Delphi_Uses extends TokenSequence
 {
 	public @S(10) Delphi_Keyword USES = new Delphi_Keyword("Uses");
-	public @S(20) SeparatedList<Delphi_Identifier_Reference,PunctuationComma> uses;
+	public @S(20) SeparatedList<Delphi_UseItem,PunctuationComma> uses;
 	public @S(30) PunctuationSemicolon semicolon;
+	
+	public static class Delphi_UseItem extends TokenSequence
+	{
+		public @S(10) Delphi_Identifier_Reference id;
+		public @S(20) @OPT Delphi_UseItemIn in;
+		
+		public static class Delphi_UseItemIn extends TokenSequence
+		{
+			public @S(10) Delphi_Keyword IN = new Delphi_Keyword("In");
+			public @S(20) Delphi_Literal name;
+			public @S(30) @OPT Delphi_UseItemInBraces braces;
+			
+			public static class Delphi_UseItemInBraces extends TokenSequence
+			{
+				public @S(10) PunctuationLeftBrace leftBrace;
+				public @S(20) Delphi_Identifier_Reference id;
+				public @S(30) PunctuationRightBrace rightBrace;
+			}
+		}
+	}
 }
