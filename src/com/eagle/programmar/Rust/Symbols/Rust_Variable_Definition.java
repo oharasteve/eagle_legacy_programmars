efaafd07b2fe121a400aa00e1146263df16b76de
Steve O'Hara <steve@oharasteve.com>
2022-07-03 23:14:26 -0500

    Add copyright

--- src/com/eagle/programmar/Rust/Symbols/Rust_Variable_Definition.java
+++ src/com/eagle/programmar/Rust/Symbols/Rust_Variable_Definition.java
@@ -1,3 +1,6 @@
+// Copyright Eagle Legacy Modernization LLC, 2010-date
+// Original author: Steven A. O'Hara, Jul 2, 2022
+
 package com.eagle.programmar.Rust.Symbols;
 
 public class Rust_Variable_Definition extends Rust_Identifier_Definition
