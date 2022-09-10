43e28ac85d3a9c61fb50782d6eee89304579582c
Steve O'Hara <steve@oharasteve.com>
2022-09-09 21:32:20 -0500

    Revert Rust changes

--- src/com/eagle/programmar/Rust/Rust_Type.java
+++ src/com/eagle/programmar/Rust/Rust_Type.java
@@ -14,36 +14,40 @@ import com.eagle.tokens.punctuation.PunctuationLeftParen;
 import com.eagle.tokens.punctuation.PunctuationRightBracket;
 import com.eagle.tokens.punctuation.PunctuationRightParen;
 
-public class Rust_Type extends TokenSequence implements AbstractType
+public class Rust_Type extends TokenChooser implements AbstractType
 {
-	public @S(10) @OPT Rust_Punctuation ampersand = new Rust_Punctuation("&");
-	public @S(20) @OPT Rust_TypeStatic typeStatic;
-	public @S(30) Rust_ActualType actualType;
-	
-	public static class Rust_TypeStatic extends TokenSequence
+	public @CHOICE static class Rust_TypePrimitive extends TokenSequence
 	{
-		public @S(10) Rust_Punctuation quote = new Rust_Punctuation("'");
-		public @S(20) Rust_Keyword STATIC = new Rust_Keyword("static");
-	}
-
-	public static class Rust_ActualType extends TokenChooser
-	{
-		public @CHOICE Rust_KeywordChoice STR = new Rust_KeywordChoice(
+		public @S(10) @OPT Rust_Punctuation ampersand = new Rust_Punctuation("&");
+		public @S(20) @OPT Rust_TypePrimitiveStatic typeStatic;
+		public @S(30) Rust_KeywordChoice PRIMITIVE = new Rust_KeywordChoice(
 				"bool",
 				"i32",
 				"str",
 				"String",
 				"usize");
 		
-		public @CHOICE static class Rust_TypeArray extends TokenSequence
+		public static class Rust_TypePrimitiveStatic extends TokenSequence
 		{
-			public @S(10) Rust_Punctuation ampersand = new Rust_Punctuation("&");
-			public @S(20) Rust_TypeStatic typeStatic;
-			public @S(30) PunctuationLeftBracket leftBracket;
-			public @S(40) PunctuationLeftParen leftParen;
-			public @S(50) Rust_Type subType;
-			public @S(60) PunctuationRightParen rightParen;
-			public @S(70) PunctuationRightBracket rightBracket;
+			public @S(10) Rust_Punctuation quote = new Rust_Punctuation("'");
+			public @S(20) Rust_Keyword STATIC = new Rust_Keyword("static");
+		}
+	}
+	
+	public @CHOICE static class Rust_TypeArray extends TokenSequence
+	{
+		public @S(10) Rust_Punctuation ampersand = new Rust_Punctuation("&");
+		public @S(20) Rust_TypeArrayStatic typeStatic;
+		public @S(30) PunctuationLeftBracket leftBracket;
+		public @S(40) PunctuationLeftParen leftParen;
+		public @S(50) Rust_Type subType;
+		public @S(60) PunctuationRightParen rightParen;
+		public @S(70) PunctuationRightBracket rightBracket;
+
+		public static class Rust_TypeArrayStatic extends TokenSequence
+		{
+			public @S(10) Rust_Punctuation quote = new Rust_Punctuation("'");
+			public @S(20) Rust_Keyword STATIC = new Rust_Keyword("static");
 		}
 	}
 }
