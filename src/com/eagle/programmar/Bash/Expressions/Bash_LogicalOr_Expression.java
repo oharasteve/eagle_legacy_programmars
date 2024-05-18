c706e13d89323315bd15f70f1ccb7fc6279ec1c9
Steve O'Hara <steve@oharasteve.com>
2024-05-18 18:44:10 -0500

    Fix Bash && || < > by using quotes

--- src/com/eagle/programmar/Bash/Expressions/Bash_LogicalOr_Expression.java
+++ src/com/eagle/programmar/Bash/Expressions/Bash_LogicalOr_Expression.java
@@ -3,13 +3,31 @@
 
 package com.eagle.programmar.Bash.Expressions;
 
+import com.eagle.core.EagleInterpreter;
+import com.eagle.core.EagleRunnable;
 import com.eagle.programmar.Bash.Bash_Expression;
 import com.eagle.programmar.Bash.Terminals.Bash_Punctuation;
 import com.eagle.tokens.PrecedenceOperator;
 
-public class Bash_LogicalOr_Expression extends PrecedenceOperator
+public class Bash_LogicalOr_Expression extends PrecedenceOperator implements EagleRunnable
 {
 	public @S(10) Bash_Expression left = new Bash_Expression(this, AllowedPrecedence.ATLEAST);
 	public @S(20) Bash_Punctuation operator = new Bash_Punctuation("||");
 	public @S(30) Bash_Expression right = new Bash_Expression(this, AllowedPrecedence.HIGHER);
+
+	@Override
+	public void interpret(EagleInterpreter interpreter)
+	{
+		boolean leftValue = interpreter.getBoolValue(left);
+		if (leftValue)
+		{
+			// Short circuit, don't bother with RHS
+			interpreter.pushBool(true);
+		}
+		else
+		{
+			boolean rightValue = interpreter.getBoolValue(right);
+			interpreter.pushBool(rightValue);
+		}
+	}
 }
