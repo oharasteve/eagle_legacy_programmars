6396b2e3c713baa8f7379e140e7d0282a6a216d0
Steve O'Hara <steve@oharasteve.com>
2024-07-19 20:03:22 -0500

    Undo last change for Javascript 'for' statement
    Messed up all sorts of javascript programs

--- src/com/eagle/programmar/Javascript/Statements/Javascript_ForStatement.java
+++ src/com/eagle/programmar/Javascript/Statements/Javascript_ForStatement.java
@@ -5,6 +5,7 @@ package com.eagle.programmar.Javascript.Statements;
 
 import com.eagle.core.EagleInterpreter;
 import com.eagle.core.EagleRunnableWithResult;
+import com.eagle.math.EagleValue;
 import com.eagle.metrics.ForLoopMetric;
 import com.eagle.metrics.ForLoopMetrics;
 import com.eagle.programmar.Javascript.Javascript_Data.Javascript_More_Variables;
@@ -12,11 +13,15 @@ import com.eagle.programmar.Javascript.Javascript_Expression;
 import com.eagle.programmar.Javascript.Javascript_Statement;
 import com.eagle.programmar.Javascript.Javascript_Type;
 import com.eagle.programmar.Javascript.Javascript_Variable;
-import com.eagle.programmar.Javascript.Expressions.Javascript_AssignmentExpression;
+import com.eagle.programmar.Javascript.Statements.Javascript_ForStatement.Javascript_ForLoopStatement.Javascript_ForLoopVariable.Javascript_ForLoopVariableNoType;
+import com.eagle.programmar.Javascript.Statements.Javascript_ForStatement.Javascript_ForLoopStatement.Javascript_ForLoopVariable.Javascript_ForLoopVariableWithType;
+import com.eagle.programmar.Javascript.Symbols.Javascript_Identifier_Reference;
 import com.eagle.programmar.Javascript.Symbols.Javascript_Variable_Definition;
 import com.eagle.programmar.Javascript.Terminals.Javascript_Comment;
 import com.eagle.programmar.Javascript.Terminals.Javascript_Keyword;
 import com.eagle.programmar.Javascript.Terminals.Javascript_KeywordChoice;
+import com.eagle.programmar.Javascript.Terminals.Javascript_Punctuation;
+import com.eagle.tokens.AbstractToken;
 import com.eagle.tokens.SeparatedList;
 import com.eagle.tokens.TokenChooser;
 import com.eagle.tokens.TokenList;
@@ -38,19 +43,20 @@ public class Javascript_ForStatement extends TokenChooser implements EagleRunnab
 	{
 		public @S(10) @DOC("js_loop_for.asp") Javascript_Keyword FOR = new Javascript_Keyword("for");
 		public @S(20) PunctuationLeftParen leftParen;
-		public @S(30) @OPT Javascript_Type varType;
-		public @S(40) @OPT Javascript_AssignmentExpression initialize;
-		public @S(50) @OPT TokenList<Javascript_More_Variables> moreVariables;
-		public @S(60) PunctuationSemicolon semicolon1;
-		public @S(70) @OPT Javascript_Expression terminateCondition;
-		public @S(80) PunctuationSemicolon semicolon2;
-		public @S(90) @OPT Javascript_Expression increment;
-		public @S(100) @OPT PunctuationComma comma;
-		public @S(110) @OPT Javascript_Expression extraIncrement;
-		public @S(120) PunctuationRightParen rightParen;
-		public @S(130) @OPT TokenList<Javascript_Comment> comments;
-		public @S(140) Javascript_Statement action;
-
+		public @S(30) @OPT Javascript_ForLoopVariable loopVar;
+		public @S(40) @OPT Javascript_Punctuation equals = new Javascript_Punctuation("=");
+		public @S(50) @OPT Javascript_Expression initialize;
+		public @S(60) @OPT TokenList<Javascript_More_Variables> moreVariables;
+		public @S(70) PunctuationSemicolon semicolon1;
+		public @S(80) @OPT Javascript_Expression terminateCondition;
+		public @S(90) PunctuationSemicolon semicolon2;
+		public @S(100) @OPT Javascript_Expression increment;
+		public @S(110) @OPT PunctuationComma comma;
+		public @S(120) @OPT Javascript_Expression extraIncrement;
+		public @S(130) PunctuationRightParen rightParen;
+		public @S(140) @OPT TokenList<Javascript_Comment> comments;
+		public @S(150) Javascript_Statement action;
+		
 		public static class Javascript_ForLoopVariable extends TokenChooser
 		{
 			public @FIRST static class Javascript_ForLoopVariableWithType extends TokenSequence
@@ -70,7 +76,7 @@ public class Javascript_ForStatement extends TokenChooser implements EagleRunnab
 	{
 		public @S(10) Javascript_Keyword FOR = new Javascript_Keyword("for");
 		public @S(20) PunctuationLeftParen leftParen;
-		public @S(30) Javascript_Type varType;
+		public @S(30) @OPT Javascript_Type varType;
 		public @S(40) @OPT Javascript_Variable forVar; // The Javascript_Type steals it ...
 		public @S(50) @OPT Javascript_ForVariables forVars;
 		public @S(60) Javascript_InOrColon inOrColon;
@@ -100,7 +106,24 @@ public class Javascript_ForStatement extends TokenChooser implements EagleRunnab
 		{
 			Javascript_ForLoopStatement forLoop = (Javascript_ForLoopStatement) this.getWhich();
 
-			interpreter.tryToInterpret(forLoop.initialize);
+			AbstractToken which = forLoop.loopVar.getWhich();
+			
+			Javascript_Variable forVar;
+			if (which instanceof Javascript_ForLoopVariableWithType)
+			{
+				forVar = ((Javascript_ForLoopVariableWithType) which).forVar;
+			}
+			else
+			{
+				forVar = ((Javascript_ForLoopVariableNoType) which).forVar;
+			}
+			
+			if (forVar.firstId.getWhich() instanceof Javascript_Identifier_Reference)
+			{
+				Javascript_Identifier_Reference id = (Javascript_Identifier_Reference) forVar.firstId.getWhich();
+				EagleValue init = interpreter.getEagleValue(forLoop.initialize);
+				interpreter._symbolTable.setSymbol(this.getFileName(), this.getStartLine(), this.getStartChar(), id.getValue(), init);
+			}
 
 			if (_metrics == null)
 			{
