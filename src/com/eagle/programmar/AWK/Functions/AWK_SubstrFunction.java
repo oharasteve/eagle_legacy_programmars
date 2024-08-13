0d5241752f092ff358dd95f6766138e1cab11988
Steve O'Hara <steve@oharasteve.com>
2024-08-13 08:13:06 -0500

    For AWK substr(), the nc parameter is optional

--- src/com/eagle/programmar/AWK/Functions/AWK_SubstrFunction.java
+++ src/com/eagle/programmar/AWK/Functions/AWK_SubstrFunction.java
@@ -19,8 +19,8 @@ public class AWK_SubstrFunction extends PrimaryOperator implements EagleRunnable
 	public @S(30) AWK_Expression expr;
 	public @S(40) PunctuationComma comma1;
 	public @S(50) AWK_Expression scExpr;
-	public @S(60) PunctuationComma comma2;
-	public @S(70) AWK_Expression ecExpr;
+	public @S(60) @OPT PunctuationComma comma2;
+	public @S(70) @OPT AWK_Expression ncExpr;
 	public @S(80) PunctuationRightParen rightParen;
 
 	@Override
@@ -28,9 +28,16 @@ public class AWK_SubstrFunction extends PrimaryOperator implements EagleRunnable
 	{
 		String strArg = interpreter.getStrValue(expr);
 		int sc = interpreter.getIntValue(scExpr) - 1;
-		int nc = interpreter.getIntValue(ecExpr);
 		if (sc > strArg.length()) throw new RuntimeException("Error on substr for " + strArg);
-		if (sc + nc > strArg.length()) nc = strArg.length() - sc;
-		interpreter.pushStr(strArg.substring(sc, sc + nc)); // AWK substr() starts with 1, not 0
+		if (ncExpr != null && ncExpr.isPresent())
+		{
+			int nc = interpreter.getIntValue(ncExpr);
+			if (sc + nc > strArg.length()) nc = strArg.length() - sc;
+			interpreter.pushStr(strArg.substring(sc, sc + nc)); // AWK substr() starts with 1, not 0
+		}
+		else
+		{
+			interpreter.pushStr(strArg.substring(sc));
+		}
 	}
 }
