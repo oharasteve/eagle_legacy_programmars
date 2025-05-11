b22cb0b37cfaa2ea95a866975ac55c31485fc1d4
Steve O'Hara <steve@oharasteve.com>
2025-05-11 11:05:43 -0500

    AWK can now run a While statement

--- src/com/eagle/programmar/AWK/Statements/AWK_WhileStatement.java
+++ src/com/eagle/programmar/AWK/Statements/AWK_WhileStatement.java
@@ -3,6 +3,10 @@
 
 package com.eagle.programmar.AWK.Statements;
 
+import com.eagle.interpret.EagleInterpreter;
+import com.eagle.interpret.EagleRunnableWithResult;
+import com.eagle.metrics.ForLoopMetric;
+import com.eagle.metrics.ForLoopMetrics;
 import com.eagle.programmar.AWK.AWK_Action;
 import com.eagle.programmar.AWK.AWK_Expression;
 import com.eagle.programmar.AWK.AWK_Statements.AWK_Statement;
@@ -13,17 +17,58 @@ import com.eagle.tokens.interfaces.AbstractStatement;
 import com.eagle.tokens.punctuation.PunctuationLeftParen;
 import com.eagle.tokens.punctuation.PunctuationRightParen;
 
-public class AWK_WhileStatement extends TokenSequence implements AbstractStatement
+public class AWK_WhileStatement extends TokenSequence
+		implements AbstractStatement, EagleRunnableWithResult
 {
 	public @S(10) @DOC("#index-while-statement-1") AWK_Keyword WHILE = new AWK_Keyword("while");
 	public @S(20) PunctuationLeftParen leftParen;
 	public @S(30) AWK_Expression condition;
 	public @S(40) PunctuationRightParen rightParen;
-	public @S(50) AWK_WhileBlock block;
+	public @S(50) AWK_WhileBlock whileStatement;
 
 	public static class AWK_WhileBlock extends TokenChooser
 	{
 		public @CHOICE AWK_Statement XXstmt;
 		public @CHOICE AWK_Action XXactions;
+	}
+	
+	private @SKIP ForLoopMetrics _metrics = null;
+
+	@Override
+	public Eagle_Statement_Result interpretStatement(EagleInterpreter interpreter)
+	{
+		if (_metrics == null)
+		{
+			_metrics = new ForLoopMetrics(interpreter._metrics, this);
+		}
+		ForLoopMetric metric = new ForLoopMetric();
+
+		Eagle_Statement_Result result = Eagle_Statement_Result.NORMAL;
+		while (true)
+		{
+			boolean keepGoing = interpreter.getBoolValue(condition);
+			if (!keepGoing) break;
+
+			metric.iterate();
+			result = interpreter.tryToInterpret(whileStatement);
+			if (result == Eagle_Statement_Result.BREAK)
+			{
+				metric.broke();
+				result = Eagle_Statement_Result.NORMAL;
+				break;
+			}
+			else if (result == Eagle_Statement_Result.CONTINUE)
+			{
+				metric.continued();
+				result = Eagle_Statement_Result.NORMAL;
+			}
+			else if (result == Eagle_Statement_Result.RETURN)
+			{
+				break;
+			}
+		}
+
+		_metrics.competedLoop(metric);
+		return result;
 	}
 }
