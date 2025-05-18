91363b8505db53e66b34b4950942330a24a5b18e
Steve O'Hara <steve@oharasteve.com>
2025-05-18 08:00:47 -0500

    Julia makes me use 'global' on assignments inside a for / while loop

--- src/com/eagle/programmar/Julia/Statements/Julia_Assignment.java
+++ src/com/eagle/programmar/Julia/Statements/Julia_Assignment.java
@@ -11,16 +11,18 @@ import com.eagle.programmar.Julia.Julia_Expression;
 import com.eagle.programmar.Julia.Julia_Variable;
 import com.eagle.programmar.Julia.Symbols.Julia_Identifier_Reference;
 import com.eagle.programmar.Julia.Terminals.Julia_EOLN;
+import com.eagle.programmar.Julia.Terminals.Julia_Keyword;
 import com.eagle.programmar.Julia.Terminals.Julia_PunctuationChoice;
 import com.eagle.tokens.TokenSequence;
 import com.eagle.tokens.interfaces.AbstractStatement;
 
 public class Julia_Assignment extends TokenSequence implements EagleRunnable, AbstractStatement
 {
-	public @S(10) Julia_Variable var;
-	public @S(20) Julia_PunctuationChoice equals = new Julia_PunctuationChoice("=", "+=", "-=", "*=", "/=", ":=");
-	public @S(30) Julia_Expression expr;
-	public @S(40) Julia_EOLN eoln;
+	public @S(10) @OPT Julia_Keyword GLOBAL = new Julia_Keyword("global");
+	public @S(20) Julia_Variable var;
+	public @S(30) Julia_PunctuationChoice equals = new Julia_PunctuationChoice("=", "+=", "-=", "*=", "/=", ":=");
+	public @S(40) Julia_Expression expr;
+	public @S(50) Julia_EOLN eoln;
 
 	@Override
 	public void interpret(EagleInterpreter interpreter)
