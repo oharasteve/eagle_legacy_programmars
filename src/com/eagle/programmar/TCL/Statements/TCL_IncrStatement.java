29df334838f42fd9ea82dfa24ea7002ac4b0bb8b
Steve O'Hara <steve@oharasteve.com>
2025-05-18 08:01:05 -0500

    TCL has no 'decr' command -- have to use incr

--- src/com/eagle/programmar/TCL/Statements/TCL_IncrStatement.java
+++ src/com/eagle/programmar/TCL/Statements/TCL_IncrStatement.java
@@ -8,13 +8,13 @@ import com.eagle.interpret.EagleRunnable;
 import com.eagle.math.EagleInteger;
 import com.eagle.programmar.TCL.TCL_Expression;
 import com.eagle.programmar.TCL.TCL_Variable;
-import com.eagle.programmar.TCL.Terminals.TCL_KeywordChoice;
+import com.eagle.programmar.TCL.Terminals.TCL_Keyword;
 import com.eagle.tokens.TokenSequence;
 import com.eagle.tokens.interfaces.AbstractStatement;
 
 public class TCL_IncrStatement extends TokenSequence implements AbstractStatement, EagleRunnable
 {
-	public @S(10) @DOC("TclCmd/incr.html") TCL_KeywordChoice INCR = new TCL_KeywordChoice("incr", "decr");
+	public @S(10) @DOC("TclCmd/incr.html") TCL_Keyword INCR = new TCL_Keyword("incr");
 	public @S(20) TCL_Variable var;
 	public @S(30) @OPT TCL_Expression amount;
 
@@ -28,20 +28,8 @@ public class TCL_IncrStatement extends TokenSequence implements AbstractStatemen
 		}
 		
 		int prev = interpreter.getIntValue(var);
-		int newV;
-		switch (INCR.getValue().toLowerCase())
-		{
-		case "incr":
-			newV = prev + x;
-			break;
-		case "decr":
-			newV = prev - x;
-			break;
-		default:
-			throw new RuntimeException("Unexpected keyword: " + INCR.getValue());
-		}
+		int newV = prev + x;
 		EagleInteger val = new EagleInteger(newV);
-
 		interpreter.setSymbol(var, var.id.getValue(), val);
 	}
 }
