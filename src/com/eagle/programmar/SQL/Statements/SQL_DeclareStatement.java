7f2c370ece164ce35643bc7d3c3e8bdb8589e8e3
Steve O'Hara <steve@oharasteve.com>
2025-10-17 22:26:27 -0500

    Transform SQL Declare as an assignment statement

--- src/com/eagle/programmar/SQL/Statements/SQL_DeclareStatement.java
+++ src/com/eagle/programmar/SQL/Statements/SQL_DeclareStatement.java
@@ -3,6 +3,8 @@
 
 package com.eagle.programmar.SQL.Statements;
 
+import java.util.ArrayList;
+
 import com.eagle.interpret.EagleInterpreter;
 import com.eagle.interpret.EagleRunnable;
 import com.eagle.math.EagleString;
@@ -14,9 +16,17 @@ import com.eagle.programmar.SQL.Terminals.SQL_Keyword;
 import com.eagle.programmar.SQL.Terminals.SQL_Punctuation;
 import com.eagle.tokens.TokenList;
 import com.eagle.tokens.TokenSequence;
+import com.eagle.tokens.interfaces.AbstractExpression;
+import com.eagle.tokens.interfaces.AbstractStatement;
 import com.eagle.tokens.punctuation.PunctuationSemicolon;
+import com.eagle.transform.EagleGenerator;
+import com.eagle.transform.EagleGenerator.AssignmentEnum;
+import com.eagle.transform.EagleGenerator.SubscriptEnum;
+import com.eagle.transform.EagleTransformableStatementList;
+import com.eagle.transform.EagleTransformer;
 
-public class SQL_DeclareStatement extends TokenSequence implements EagleRunnable
+public class SQL_DeclareStatement extends TokenSequence
+		implements EagleRunnable, EagleTransformableStatementList
 {
 	public @S(10) SQL_Keyword DECLARE = new SQL_Keyword("DECLARE");
 	public @S(20) TokenList<SQL_Declaration> declarations;
@@ -42,5 +52,25 @@ public class SQL_DeclareStatement extends TokenSequence implements EagleRunnable
 			}
 			interpreter.setSymbol(decl.id, decl.id.toString(), value);
 		}
+	}
+	@Override
+	public ArrayList<AbstractStatement> transformStatement(EagleTransformer transformer, EagleGenerator generator)
+	{
+		ArrayList<AbstractStatement> result = new ArrayList<AbstractStatement>();
+		
+		for (SQL_Declaration decl : declarations._elements)
+		{
+			String varName = decl.id.getValue();
+			AbstractExpression newVal;
+			if (decl.initialValue != null && decl.initialValue.isPresent())
+			{
+				newVal = transformer.transformExpression(generator, decl.initialValue);
+				AbstractExpression asgExpr = generator.newAssignmentExpression(varName,
+						SubscriptEnum.FIRST_IS_ZERO, null, AssignmentEnum.EQUALS, newVal, decl.initialValue);
+				result.add(generator.newExpressionStatement(asgExpr, decl.initialValue));
+			}
+		}
+		
+		return result;
 	}
 }
