c8af5c59398705d3d52e08cd9d6063eabfd5c1a9
Steve O'Hara <steve@oharasteve.com>
2026-02-27 00:31:52 -0600

    Add isString() static method

--- src/com/eagle/programmar/Rust/Expressions/Rust_EqualityExpression.java
+++ src/com/eagle/programmar/Rust/Expressions/Rust_EqualityExpression.java
@@ -5,11 +5,13 @@ package com.eagle.programmar.Rust.Expressions;
 
 import com.eagle.interpret.EagleInterpreter;
 import com.eagle.interpret.EagleRunnable;
+import com.eagle.math.EagleString;
 import com.eagle.math.EagleValue;
 import com.eagle.metrics.Operator2Metrics;
 import com.eagle.metrics.Operator2Metrics.Oper2Types;
 import com.eagle.programmar.Rust.Rust_Expression;
 import com.eagle.programmar.Rust.Rust_Generator;
+import com.eagle.programmar.Rust.Terminals.Rust_Literal;
 import com.eagle.programmar.Rust.Terminals.Rust_PunctuationChoice;
 import com.eagle.tokens.AbstractToken;
 import com.eagle.tokens.PrecedenceOperator;
@@ -25,6 +27,7 @@ public class Rust_EqualityExpression extends PrecedenceOperator
 	public @S(10) Rust_Expression left = new Rust_Expression(this, AllowedPrecedence.ATLEAST);
 	public @S(20) Rust_PunctuationChoice operator = new Rust_PunctuationChoice("==", "!=");
 	public @S(30) Rust_Expression right = new Rust_Expression(this, AllowedPrecedence.HIGHER);
+	
 	private @SKIP Operator2Metrics _metrics = null;
 
 	@Override
@@ -120,10 +123,36 @@ public class Rust_EqualityExpression extends PrecedenceOperator
 			throw new RuntimeException("Unexpected relational operator: " + operator);
 		}
 	}
+
+	private static boolean isString(AbstractExpression expression)
+	{
+		Rust_Expression expr = (Rust_Expression) expression;
+		if (expr.getWhich() instanceof Rust_Literal) return true;
+		return false;
+	}
+
 	public static Rust_Expression generateEquality(Oper2Types types, Rust_Expression leftExpr, RelationalEnum relOp,
 			Rust_Expression rightExpr, AbstractToken source)
 	{
 		Rust_EqualityExpression eqExpr = new Rust_EqualityExpression();
+		
+		boolean doStrings = false;
+		if (types != null)
+		{
+			if (types._type1.equals(EagleString.STRING) && types._type2.equals(EagleString.STRING))
+			{
+				doStrings = true;
+			}
+		}
+
+		if (!doStrings)
+		{
+			if (isString(leftExpr) || isString(rightExpr))
+			{
+				doStrings = true;
+			}
+		}
+
 		eqExpr.left = leftExpr;
 		eqExpr.right = rightExpr;
 		String oper;
