1e8f5f2b9757cd08cdcc846db64426adf9b845ad
Steve O'Hara <steve@oharasteve.com>
2026-04-13 09:34:01 -0500

    No action needed for interpreting or transforming

--- src/com/eagle/programmar/VB/Statements/VB_OptionStatement.java
+++ src/com/eagle/programmar/VB/Statements/VB_OptionStatement.java
@@ -7,9 +7,16 @@ import com.eagle.interpret.EagleInterpreter;
 import com.eagle.interpret.EagleRunnable;
 import com.eagle.programmar.VB.Terminals.VB_Keyword;
 import com.eagle.tokens.TokenSequence;
+import com.eagle.tokens.interfaces.AbstractExpression;
+import com.eagle.tokens.interfaces.AbstractStatement;
+import com.eagle.tokens.interfaces.AbstractType;
+import com.eagle.tokens.interfaces.AbstractVariable;
+import com.eagle.transform.EagleGenerator;
+import com.eagle.transform.EagleTransformableStatement;
+import com.eagle.transform.EagleTransformer;
 
 public class VB_OptionStatement extends TokenSequence
-		implements EagleRunnable
+		implements EagleRunnable, EagleTransformableStatement
 {
 	public @S(10) @DOC("statements/option-keyword-statement") VB_Keyword OPTION = new VB_Keyword("option");
 	public @S(20) VB_Keyword EXPLICIT = new VB_Keyword("explicit");
@@ -18,5 +25,13 @@ public class VB_OptionStatement extends TokenSequence
 	public void interpret(EagleInterpreter interpreter)
 	{
 		// Ignore for now
+	}
+
+	@Override
+	public AbstractStatement transformStatement(EagleTransformer transformer,
+			EagleGenerator<AbstractStatement, AbstractExpression, AbstractVariable, AbstractType> generator)
+	{
+		// Nothing worth doing
+		return null;
 	}
 }
