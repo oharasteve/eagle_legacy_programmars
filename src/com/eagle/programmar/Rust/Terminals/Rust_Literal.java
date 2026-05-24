6f854286d50098bd79ff4e506535a839cb734cf8
Steve O'Hara <steve@oharasteve.com>
2026-05-24 08:55:57 -0600

    Rust_Literal is very confusing with all the \

--- src/com/eagle/programmar/Rust/Terminals/Rust_Literal.java
+++ src/com/eagle/programmar/Rust/Terminals/Rust_Literal.java
@@ -19,9 +19,9 @@ public class Rust_Literal extends TerminalLiteralToken
 	{
 		Rust_Literal lit = new Rust_Literal();
 		String val = '"' + value
-//				.replaceAll("\\\\", "\\\\\\\\")
-				.replaceAll("\\\"", "\\\\\"")
-				.replaceAll("\n", "\\n") + '"';
+				.replaceAll("\\\\n", "\n")
+				.replaceAll("\\\"", "\\\"")
+				.replaceAll("\\\\", "\\\\\\\\") + '"';
 		lit.setValue(val);
 		lit.setTransformationSource(source);
 		return lit;
