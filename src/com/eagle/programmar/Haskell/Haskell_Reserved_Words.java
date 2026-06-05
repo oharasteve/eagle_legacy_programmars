69a2dc8a3b9e91fec1b60770465a2ac589f6bc3f
Steve O'Hara <steve@oharasteve.com>
2026-06-05 07:42:01 -0500

    Add copyright

--- src/com/eagle/programmar/Haskell/Haskell_Reserved_Words.java
+++ src/com/eagle/programmar/Haskell/Haskell_Reserved_Words.java
@@ -1,3 +1,6 @@
+// Copyright Eagle Legacy Modernization LLC, 2010-date
+// Original author: Steven A. O'Hara, Jun 4, 2026
+
 package com.eagle.programmar.Haskell;
 
 public class Haskell_Reserved_Words
