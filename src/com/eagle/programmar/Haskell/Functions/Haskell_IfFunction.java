8d3491a4967f1fb196ccd4efb7686eb58d83ff30
Steve O'Hara <steve@oharasteve.com>
2026-06-08 23:31:48 -0500

    Working on Haskell "if" expression

--- src/com/eagle/programmar/Haskell/Functions/Haskell_IfFunction.java
+++ src/com/eagle/programmar/Haskell/Functions/Haskell_IfFunction.java
@@ -6,6 +6,7 @@ package com.eagle.programmar.Haskell.Functions;
 import com.eagle.interpret.EagleInterpreter;
 import com.eagle.interpret.EagleRunnable;
 import com.eagle.programmar.Haskell.Haskell_Expression;
+import com.eagle.programmar.Haskell.Terminals.Haskell_EndOfLine;
 import com.eagle.programmar.Haskell.Terminals.Haskell_Keyword;
 import com.eagle.tokens.PrimaryOperator;
 
@@ -14,10 +15,12 @@ public class Haskell_IfFunction extends PrimaryOperator
 {
 	public @S(10) Haskell_Keyword IF = new Haskell_Keyword("if");
 	public @S(20) Haskell_Expression condition;
-	public @S(30) Haskell_Keyword THEN = new Haskell_Keyword("then");
-	public @S(40) Haskell_Expression thenExpr;
-	public @S(50) Haskell_Keyword ELSE = new Haskell_Keyword("else");
-	public @S(60) Haskell_Expression elseExpr;
+	public @S(30) @OPT Haskell_EndOfLine eoln1;
+	public @S(40) Haskell_Keyword THEN = new Haskell_Keyword("then");
+	public @S(50) Haskell_Expression thenExpr;
+	public @S(60) @OPT Haskell_EndOfLine eoln2;
+	public @S(70) Haskell_Keyword ELSE = new Haskell_Keyword("else");
+	public @S(80) Haskell_Expression elseExpr;
 
 	@Override
 	public void interpret(EagleInterpreter interpreter)
